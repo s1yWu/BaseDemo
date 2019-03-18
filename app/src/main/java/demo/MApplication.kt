ae071e96bcea9c75f3e1ebc9fb3f3aeb95a4bb08
s1y <1172592745@qq.com>
2019-03-18 09:30:14 +0800

    update

---
+++ app/src/main/java/demo/MApplication.kt
@@ -0,0 +1,13 @@
+package demo
+
+import android.app.Application
+
+/**
+ * Created by s1y on 2019/3/18.
+ */
+class MApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
